896a5715b1bc6425d2d1d9f65c5c6ee91d15cc8d
pmaidan <94752556+pmaidan@users.noreply.github.com>
2024-09-10 19:54:56 -0500

    Add files via upload

--- lab4.cpp
+++ lab4.cpp
@@ -10,18 +10,18 @@
 // This program calculates the theater's gross and net box office profit for a night.
 #include <iostream> // Header file iostream allows the use of cin/cout objects.
 #include <iomanip>  // Header file iomanip allows the output manipulation.
-#include <string>   // Header file string provides support for manipulating strings.
-using namespace std;    // Header string allows to work with string class.
+#include <string>   // Header string allows to work with string class.
+using namespace std;    // Use the names fir objects and variables from the std library.
 
 int main()  // Main function is a starting point of the program execution.
 {
     // Declare variables, constants and strings.
-    string movieName;   // Declare a string hoding a movie name.
-    const double NET_PERCENTILE = 0.2;  // Initialize the constant percent of gross income kept.
-    int adultCount, childCount; // Declare the variables of integer type.
-    int adultTicketPrice, childTicketPrice; // Declare the variables of integer type.
-    double adultProfit, childProfit;    // Declare the variables or double type.
-    double grossProfit, netProfit, distributorProfit;   // Declare the variables or double type.
+    string movieName;   // Declare a string holding a movie name.
+    const double GROSS_PERCENT = 0.2;  // Initialize the constant percent of gross income kept.
+    int adultCount, childCount; // Declare the variables for the number of adult and child tickets sold.
+    double adultTicketPrice, childTicketPrice; // Declare the variables for the adult and child tickets price.
+    double adultProfit, childProfit;    // Declare the variables for income from the adult and child tickets sold.
+    double grossProfit, netProfit, distributorProfit;   // Declare the variables for profit.
 
     //Get the input.
     cout << "Enter the name of the movie: ";    // Display the string, requesting input.
@@ -37,32 +37,48 @@ int main()  // Main function is a starting point of the program execution.
     cout << "ranging from $9 to $12: "; // Display the string, requesting input.
     cin >> adultTicketPrice;    // Get input and store it in adultTicketPrice.
 
+    // Check if input is in the specified range.
+    // "If" statement specifies a block of code to be executed if a condition is true.
+    if (adultTicketPrice < 9 || adultTicketPrice > 12)  // Check if the input is out of range.
+    {
+        cout << "Invalid adult ticket price. Please enter a value between $9 and $12." << endl;    // Display an error statement.
+        return -1;  // Return -1 to indicate an error in the input for adult ticket price.
+    }
+
+    // Get the input.
     cout << "Enter the price of a child ticket, ";  // Display the string, requesting input.
     cout << "ranging from $3 to $6: ";  // Display the string, requesting input
     cin >> childTicketPrice;    // Get input and store it in childTicketPrice.
 
+    // Check if input is in the specified range.
+    if (childTicketPrice < 3 || childTicketPrice > 6)   // Check if input is out of range.
+    {
+        cout << "Invalid child ticket price. Please enter a value between $3 and $6." << endl;    // Display an error statement.
+        return -2;  // Return -2 to indicate an error in the input for child ticket price.
+    }
+
     // Calculate the profit for each type of ticket.
-    adultProfit = adultTicketPrice * adultCount;    // Assign the value to adultProfit.
-    childProfit = childTicketPrice * childCount;    // Assign the value to childProfit.
+    adultProfit = adultTicketPrice * adultCount;    // Calculate the profit from adult tickets sold.
+    childProfit = childTicketPrice * childCount;    // Calculate the profit from child tickets sold.
 
     // Calculate the gross profit.
-    grossProfit = adultProfit + childProfit;    // Assign the value to grossProfit.
+    grossProfit = adultProfit + childProfit;    // Calculate the gross profit.
 
     // Calculate the net profit.
-    netProfit = grossProfit *  NET_PERCENTILE;  // Assign the value to netProfit.
+    netProfit = grossProfit *  GROSS_PERCENT;  // Calculate the net profit.
 
     // Calculate the distributor's profit.
-    distributorProfit = grossProfit - netProfit;    // Assign the value to distributorProfit.
+    distributorProfit = grossProfit - netProfit;    // Calculate the amount paid to the distributor.
 
     // Display the results.
-    cout << "Movie Name: " << movieName << endl;    // Display the string literal and variable.
-    cout << "Adult Tickets Sold: " << adultCount << endl;   // Display the string literal and variable.
-    cout << "Child Tickets Sold: " << childCount << endl;   // Display the string literal and variable.
-    cout << setprecision(2) << fixed;   // Manipulate the output to display the result to 2 decimal places.
-    cout << "Gross Box Office Profit: $" << grossProfit << endl;    // Display the string literal and variable.
-    cout << "Net Box Office Profit: $" << netProfit << endl;    // Display the string literal and variable.
-    cout << "Amount Paid to Distributor: $";    //Display the string literal.
-    cout << distributorProfit << endl;  // Display the variable.
+    cout << "Movie Name: " << movieName << endl;    // Display the name of the movie.
+    cout << "Adult Tickets Sold: " << adultCount << endl;   // Display the amount of adult tickets sold.
+    cout << "Child Tickets Sold: " << childCount << endl;   // Display the amount of child tickets sold.
+    cout << setprecision(2) << fixed;   // Display the output to 2 decimal places.
+    cout << "Gross Box Office Profit: $" << grossProfit << endl;    // Display the gross profit.
+    cout << "Net Box Office Profit: $" << netProfit << endl;    // Display the net profit.
+    cout << "Amount Paid to Distributor: $";    //Display the amount paid to the distributor.
+    cout << distributorProfit << endl;
 
     return 0;   // Return 0 to the operating system to indicate successful execution of the program.
-}   // End of the main function.
+}
